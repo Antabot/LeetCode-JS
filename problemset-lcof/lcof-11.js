200941649b9712489d87b324bb32af12056bab2a
Evan <evan_nightly@163.com>
2021-03-03 22:59:23 +0800

    day08: lcof-11

---
+++ problemset-lcof/lcof-11.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} numbers
+ * @return {number}
+ */
+var minArray = function(numbers) {
+    for(let i = 0 ; i < numbers.length - 1; i++) {
+        if(numbers[i] > numbers[i + 1]) {
+            return numbers[i + 1];
+        }
+    }
+
+    return numbers[0];
+};
