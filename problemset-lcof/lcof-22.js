3b6d8b57f3189aa73d2a3874ad3a4ee5d868aa4a
Evan <evan_nightly@163.com>
2021-03-16 20:24:36 +0800

    day19: lcof-22

---
+++ problemset-lcof/lcof-22.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var getKthFromEnd = function(head, k) {
+    let former = head;
+    let latter = head;
+
+    for(let i = 0; i < k; i++) {
+        former = former.next;
+    }
+
+    while(former) {
+        former = former.next;
+        latter = latter.next;
+        if(!former) break;
+    }
+
+    return latter;
+};
